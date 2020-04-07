4d7b46cb614da16e86491b71283a95c74a77cece
Lana Chung <lana@Lanaui-MacBookPro.local>
2020-04-07 17:12:40 +0900

    first commit

---
+++ get_next_line/get_next_line.c
@@ -0,0 +1,14 @@
+int get_next_line(int fd, char **line)
+{
+	static char *rest;
+	int BUFFER_SIZE = 10;
+	char BUF[BUFFER_SIZE + 1];
+	
+
+	if rest()
+	{
+
+	}
+
+	while (ret = read(fd, BUF, BUFFER_SIZE));
+}
