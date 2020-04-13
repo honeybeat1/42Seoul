a0911e7dac919a63a7ca24e100893abb84fad909
Lana Chung <lana@Lanaui-MacBookPro.local>
2020-04-13 17:36:44 +0900

    2 error function check

--- Libft/ft_memcmp.c
+++ Libft/ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: dachung <dachung@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/05 19:09:42 by dachung           #+#    #+#             */
-/*   Updated: 2020/04/07 19:01:54 by dachung          ###   ########.fr       */
+/*   Updated: 2020/04/13 17:30:05 by dachung          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,6 @@ int		ft_memcmp(const void *s1, const void *s2, size_t n)
 
 	s1_c = (unsigned char *)s1;
 	s2_c = (unsigned char *)s2;
-	if (ft_strlen((char *)s1) == 0 && ft_strlen((char *)s2) == 0)
-		return (0);
 	while (n-- > 0)
 	{
 		if (*s1_c != *s2_c)
--- Libft/ft_strnstr.c
+++ Libft/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: dachung <dachung@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/09 00:26:42 by dachung           #+#    #+#             */
-/*   Updated: 2020/04/07 20:06:48 by dachung          ###   ########.fr       */
+/*   Updated: 2020/04/13 17:25:58 by dachung          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,25 +16,24 @@ char		*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	char	*hay;
 	char	*hay_cpy;
+	int		i;
 	int		j;
 
 	if (*needle == 0)
 		return ((char *)haystack);
-	hay_cpy = ft_substr(haystack, 0, len);
-	while (len-- > 0)
+	i = len;
+	hay_cpy = (char *)haystack;
+	while (i-- > 0)
 	{
 		hay = ft_strchr(hay_cpy, needle[0]);
-		if (hay != NULL)
+		if (hay != NULL && len >= (hay - haystack) + (size_t)ft_strlen(needle))
 		{
 			j = -1;
 			while (needle[++j] != 0)
 				if (hay[j] != needle[j])
 					break ;
 			if (needle[j] == 0)
-			{
-				hay = ft_strchr(haystack, *hay);
 				return (hay);
-			}
 		}
 		hay_cpy++;
 	}
