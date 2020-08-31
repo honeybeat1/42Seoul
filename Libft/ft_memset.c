39067dcf88f794f19b0feebcfdfb3cd6c31cc8d7
Dahyun Chung <honeybeat1@gmail.com>
2020-08-31 20:51:00 +0900

    add ft_memset.c

---
+++ Libft/ft_memset.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dachung <dachung@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/04/07 19:02:53 by dachung           #+#    #+#             */
+/*   Updated: 2020/12/17 18:09:47 by dachung          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memset(void *s, int c, size_t n)
+{
+	unsigned char *char_ptr;
+
+	char_ptr = (unsigned char *)s;
+	while (n-- > 0)
+		*char_ptr++ = c;
+	return (s);
+}
