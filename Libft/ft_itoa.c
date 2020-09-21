0992d6f966e9c2c432c53a2ba01dd007136c7177
Dahyun Chung <honeybeat1@gmail.com>
2020-09-21 20:18:29 +0900

    add ft_itoa

---
+++ Libft/ft_itoa.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dachung <dachung@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/03/22 22:10:35 by dachung           #+#    #+#             */
+/*   Updated: 2020/04/09 21:39:57 by dachung          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int		get_len(long nb)
+{
+	int		len;
+
+	len = 0;
+	if (nb <= 0)
+	{
+		nb = nb * -1;
+		len++;
+	}
+	while (nb > 0)
+	{
+		nb = nb / 10;
+		len++;
+	}
+	return (len);
+}
+
+char	*ft_itoa(int nb)
+{
+	char	*str;
+	long	n;
+	int		i;
+
+	n = nb;
+	i = get_len(n);
+	if (!(str = (char*)malloc(sizeof(char) * (i + 1))))
+		return (NULL);
+	str[i--] = '\0';
+	if (n == 0)
+	{
+		str[0] = 48;
+		return (str);
+	}
+	if (n < 0)
+	{
+		str[0] = '-';
+		n = n * -1;
+	}
+	while (n > 0)
+	{
+		str[i--] = 48 + (n % 10);
+		n = n / 10;
+	}
+	return (str);
+}
