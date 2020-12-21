683d606e9af1b152a2223dae93a0ffd0a89c6bf9
Dahyun Chung <honeybeat1@gmail.com>
2020-12-22 01:57:48 +0900

    check norm

--- exam/exam02/union.c
+++ exam/exam02/union.c
@@ -6,13 +6,13 @@
 /*   By: dachung <dachung@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/22 00:28:05 by dachung           #+#    #+#             */
-/*   Updated: 2020/12/22 01:48:13 by dachung          ###   ########.fr       */
+/*   Updated: 2020/12/22 01:57:04 by dachung          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int		check_double(char* str, int max_pos, char c)
+int		check_double(char *str, int max_pos, char c)
 {
 	int	i;
 
@@ -26,8 +26,7 @@ int		check_double(char* str, int max_pos, char c)
 	return (1);
 }
 
-
-void	ft_union(char* str1, char* str2)
+void	ft_union(char *str1, char *str2)
 {
 	int	i;
 	int	j;
@@ -52,6 +51,6 @@ int		main(int argc, char **argv)
 {
 	if (argc == 3)
 		ft_union(argv[1], argv[2]);
-	write(1,"\n",1);
+	write(1, "\n", 1);
 	return (0);
 }
